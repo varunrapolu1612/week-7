b1a44b24d2fdde3d365fcad7d5b797289d11ca59
leon todd johnson <126616861+leontoddjohnson@users.noreply.github.com>
2025-10-01 19:15:29 +0000

    use exercises.ipynb to check unit tests

--- test_loader.py
+++ test_loader.py
@@ -3,16 +3,14 @@ import pandas as pd
 from loader import *
 
 class TestLoader(unittest.TestCase):
-    def __init__(self):
-        # maintain the same geolocator session throughout tests
-        self.geolocator = get_geolocator()
-
     def test_valid_locations(self):
         # your code here
         return None
 
     def test_invalid_location(self):
-        result = fetch_location_data(self.geolocator, "asdfqwer1234")
+        geolocator = get_geolocator()
+        result = fetch_location_data(geolocator, "asdfqwer1234")
+
         self.assertIsNone(result, 
                           "A nonexistent location should have an empty result.")
 
